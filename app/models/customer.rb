880dab71c26a8ae7a78a785ae2a7ea19b1b38e3e
Maximilian Wagner <maw3350@gmail.com>
2022-07-28 00:27:50 -0400

    feat: add models and testing for customers and subscriptions

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  validates :email, uniqueness: true
+  validates_presence_of :first_name, :last_name, :email, :address
+
+  has_many :subscriptions
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,9 @@
 class Subscription < ApplicationRecord
+  enum status: { active: 0, cancelled: 1 }
+  enum frequency: { weekly: 0, monthly: 1, annually: 2 }
+
+  validates_presence_of :title, :price, :status, :frequency
+  validates_numericality_of :price
+
   belongs_to :customer
 end
